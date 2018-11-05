987de842aa16391936fa0954c719a8f9d51c3cfa
Sante Stanisci <sante.stanisci@hotmail.it>
2018-11-05 16:58:00 +0100

    remove test

--- src/main/java/it/android/hal/test/Employee.java
+++
@@ -1,6 +0,0 @@
-package it.android.hal.test;
-
-import it.android.hal.resource.Resource;
-
-public class Employee extends Resource {
-}
--- src/main/java/it/android/hal/test/EmployeeLiveData.java
+++
@@ -1,6 +0,0 @@
-package it.android.hal.test;
-
-import it.android.hal.livedata.RestLiveData;
-
-public class EmployeeLiveData extends RestLiveData<Employee> {
-}
